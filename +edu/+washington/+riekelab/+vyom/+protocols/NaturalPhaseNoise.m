a51f5631df029066555de283ab4553e347a44e01
Vyom Raval <47093961+vmr160030@users.noreply.github.com>
2023-06-12 14:30:02 -0700

    Copying present images, adding aperture

--- +edu/+washington/+riekelab/+vyom/+protocols/NaturalPhaseNoise.m
+++ +edu/+washington/+riekelab/+vyom/+protocols/NaturalPhaseNoise.m
@@ -1,124 +1,97 @@
+% Loads images for MEA
 classdef NaturalPhaseNoise < manookinlab.protocols.ManookinLabStageProtocol
+    
     properties
-        amp                             % Output amplifier
-        preTime = 100                   % Stimulus leading duration (ms)
-        stimTime = 100                 % Stimulus duration (ms)
-        tailTime = 100                  % Stimulus trailing duration (ms)
-        stimNameFile = 'C:\Users\Public\Documents\GitRepos\Symphony2\vyom-package\+edu\+washington\+riekelab\+vyom\+Doves\+PhaseNoiseImages\pn_June6_2023.csv' % csv of stimulus filenames
-        noiseIndices = [0 1 2 3 4] % Phase noise amplitude index (0:4)
-        maskDiameter = 0                % Mask diameter in pixels
+        amp % Output amplifier
+        preTime     = 250 % in ms
+        stimTime    = 250 % in ms
+        tailTime    = 250 % in ms
         apertureDiameter = 2000         % Aperture diameter in pixels.
-        freezeFEMs = false
-        onlineAnalysis = 'extracellular' % Type of online analysis
-        numberOfRepeats = uint16(10)   % Number of flashes for each image
-        modImgDir = 'C:\Users\Public\Documents\GitRepos\Symphony2\vyom-package\+edu\+washington\+riekelab\+vyom\+Doves\+PhaseNoiseImages\'  % Directory of modified image .mat files
+        fileFolder = 'flashImages'; % Folder in freedland-package containing videos
+        backgroundIntensity = 0.5; % 0 - 1 (corresponds to image intensities in folder)
+        randomize = true; % whether to randomize movies shown
+        % Additional parameters
+        onlineAnalysis = 'none'
+        numberOfAverages = uint16(5) % number of epochs to queue
+        
     end
     
     properties (Hidden)
-        numImages
-        numNoiseAmps
-        numVariants
-        numberOfAverages % Number of epochs
         ampType
-        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'spikes_CClamp', 'subthresh_CClamp', 'analog'})
-        imgData
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'}) 
+        sequence
+        imagePaths
         imageMatrix
-        backgroundIntensity
-        imageName
-        imageNames
-        magnificationFactor
-        currentStimSet
-        stimCsv
-        stimListIndex
-        noiseIndex
-        pkgDir
-        im
+        directory
+        totalRuns
+        image_name
+        seed
     end
-    
+
     methods
         
         function didSetRig(obj)
             didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
-            
             [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
         end
-        
+
         function prepareRun(obj)
-            % Get the magnification factor. Exps were done with each pixel
-            % = 1 arcmin == 1/60 degree; 200 um/degree...
-            obj.magnificationFactor = round(1/60*200/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'));
-            
-            % Load stimulus info csv
-            obj.stimCsv = readtable(obj.stimNameFile);
-            obj.imageNames = unique(obj.stimCsv.img_name);
-            obj.numImages = length(obj.imageNames);
-            disp(['Num images ', num2str(obj.numImages)]);
-            obj.numNoiseAmps = length(obj.noiseIndices);
-            obj.numVariants = obj.numNoiseAmps + 1;
-            disp(obj.numVariants);
-            obj.numberOfAverages = obj.numberOfRepeats * obj.numVariants * obj.numImages;
-            
-            % Prepare run superclass later as needs numberOfAverages set
+
             prepareRun@manookinlab.protocols.ManookinLabStageProtocol(obj);
 
-            % Set noise and stimulus indices.
-            obj.noiseIndex = -2;
-            obj.stimListIndex = 1;
-            obj.getImageData();
-
-            disp('Prepared run');
-        end
-        
-        function getImageData(obj)
-            % Get the image name.
-            obj.imageName = obj.imageNames(obj.stimListIndex);
-            filepath = char(strcat(obj.modImgDir, obj.imageName, '.mat'));
-            disp(filepath);
-            obj.imgData = load(filepath);
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
             
-        end
-        
-        function getImageVersion(obj)
-            % Load the image if noise index is 0.
-            if obj.noiseIndex == -1
-                obj.imageMatrix = obj.imgData.base;
+            % General directory
+            obj.directory = strcat('C:\Users\Public\Documents\GitRepos\Symphony2\flashed_images\',obj.fileFolder); % General folder
+            D = dir(obj.directory);
+            
+            obj.imagePaths = cell(size(D,1),1);
+            for a = 1:length(D)
+                if sum(strfind(D(a).name,'.png')) > 0
+                    obj.imagePaths{a,1} = D(a).name;
+                end
             end
-
-            % If noiseIndex>0, load the noise image.
-            if obj.noiseIndex >= 0
-                obj.imageMatrix = obj.imgData.(['noise_', num2str(obj.noiseIndex)]);
-            end    
+            obj.imagePaths = obj.imagePaths(~cellfun(@isempty, obj.imagePaths(:,1)), :);
+            
+            num_reps = ceil(double(obj.numberOfAverages)/size(obj.imagePaths,1));
+            obj.sequence = (1:size(obj.imagePaths,1))' * ones(1,num_reps);
+            obj.sequence = obj.sequence(:);
+            
+            if obj.randomize
+                obj.seed = RandStream.shuffleSeed;
+                noiseStream = RandStream('mt19937ar', 'Seed', obj.seed);
+                obj.sequence = obj.sequence(noiseStream.randperm(length(obj.sequence)));
+            end
             
         end
+
         
         function p = createPresentation(obj)
+            
+            % Stage presets
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();     
             p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
-            %canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
-            p.setBackgroundColor(obj.backgroundIntensity);
             
-            % Create your scene.
-            scene = stage.builtin.stimuli.Image(obj.imageMatrix);
-            scene.size = [size(obj.imageMatrix,2) size(obj.imageMatrix,1)]*obj.magnificationFactor;
-            %scene.size = canvasSize;
-            p0 = obj.canvasSize/2;
-            scene.position = p0;
+            % Rotate image
+            specificImage = imread(fullfile(obj.directory, obj.image_name));
+            p.setBackgroundColor(obj.backgroundIntensity)   % Set background intensity
             
+            % Prep to display image
+            scene = stage.builtin.stimuli.Image(uint8(specificImage));
+            scene.size = [canvasSize(1),canvasSize(2)];
+            scene.position = canvasSize/2;
+            
+            % Use linear interpolation when scaling the image
             scene.setMinFunction(GL.LINEAR);
             scene.setMagFunction(GL.LINEAR);
-            
-            % Add the stimulus to the presentation.
-            p.addStimulus(scene);            
 
+            % Only allow image to be visible during specific time
+            p.addStimulus(scene);
             sceneVisible = stage.builtin.controllers.PropertyController(scene, 'visible', ...
                 @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
             p.addController(sceneVisible);
             
-            %--------------------------------------------------------------
-            % Size is 0 to 1
-            sz = (obj.apertureDiameter)/min(obj.canvasSize);
-            % Create the outer mask.
-            if sz < 1
-                aperture = stage.builtin.stimuli.Rectangle();
+            aperture = stage.builtin.stimuli.Rectangle();
                 aperture.position = obj.canvasSize/2;
                 aperture.color = obj.backgroundIntensity;
                 aperture.size = obj.canvasSize;
@@ -129,64 +102,27 @@ classdef NaturalPhaseNoise < manookinlab.protocols.ManookinLabStageProtocol
                 mask = stage.core.Mask(circle);
                 aperture.setMask(mask);
                 p.addStimulus(aperture); %add aperture
-            end
-            
-            if (obj.maskDiameter > 0) % Create mask
-                mask = stage.builtin.stimuli.Ellipse();
-                mask.position = obj.canvasSize/2;
-                mask.color = obj.backgroundIntensity;
-                mask.radiusX = obj.maskDiameter/2;
-                mask.radiusY = obj.maskDiameter/2;
-                p.addStimulus(mask); %add mask
-            end
-            disp('created presentation');
         end
         
         function prepareEpoch(obj, epoch)
             prepareEpoch@manookinlab.protocols.ManookinLabStageProtocol(obj, epoch);
-            % Increment noise index till end of noiseAmps
-            obj.noiseIndex = obj.noiseIndex + 1;
-            if obj.noiseIndex <= obj.numNoiseAmps
-                obj.getImageVersion();
+            
+            img_name = obj.sequence(mod(obj.numEpochsCompleted,length(obj.sequence)) + 1);
+            obj.image_name = obj.imagePaths{img_name,1};
+            
+            epoch.addParameter('imageName',obj.imagePaths{img_name,1});
+            epoch.addParameter('folder',obj.directory);
+            if obj.randomize
+                epoch.addParameter('seed',obj.seed);
             end
-            
-            % At end of noiseAmps, increment stimIndex
-            if obj.noiseIndex > obj.numNoiseAmps
-                obj.noiseIndex = -1;
-                obj.stimListIndex = obj.stimListIndex + 1;
-                if obj.stimListIndex > obj.numImages
-                   obj.stimListIndex = 1; 
-                end
-
-                % Set the current stimulus
-                obj.getImageData();
-                obj.getImageVersion();
-            end
-            
-            %imshow(obj.imageMatrix);
-            
-            % Save the parameters.
-            epoch.addParameter('stimListIndex', obj.stimListIndex);
-            epoch.addParameter('imageName', obj.imageName);
-            epoch.addParameter('backgroundIntensity', obj.backgroundIntensity);
-            epoch.addParameter('magnificationFactor', obj.magnificationFactor);
-            epoch.addParameter('stimNameFile',obj.stimNameFile);
-            epoch.addParameter('noiseIndex', obj.noiseIndex);
-            disp('Prepared epoch');
-            
-            % Display stim and noise indices
-            disp(['Stimulus index: ' num2str(obj.stimListIndex)]);            
-            disp(obj.imageName);
-            disp(['Noise index: ' num2str(obj.noiseIndex)]);
         end
-        
-        
+
         function tf = shouldContinuePreparingEpochs(obj)
-            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+            tf = obj.numEpochsPrepared < length(obj.sequence);
         end
         
         function tf = shouldContinueRun(obj)
-            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+            tf = obj.numEpochsCompleted < length(obj.sequence);
         end
     end
 end
