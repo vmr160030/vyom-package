c6019439f48e0448f782dfe621b53cd69614b352
Vyom Raval <vyomr@uw.edu>
2024-12-09 16:00:11 -0800

    just need to create image once

--- +edu/+washington/+riekelab/+vyom/+protocols/SpatialGap.m
+++ +edu/+washington/+riekelab/+vyom/+protocols/SpatialGap.m
@@ -55,57 +55,53 @@ classdef SpatialGap < manookinlab.protocols.ManookinLabStageProtocol
             obj.seqSpatialGaps = repmat(obj.seqSpatialGaps, 1, obj.numberOfRepeats);
             obj.seqContrasts = repmat(obj.seqContrasts, 1, obj.numberOfRepeats);
         end
+
+        function imageMatrix = computeImage(obj)
+            % Create a hollow circle with a gap
+            outerRadius = obj.circleRadiusPix;
+            innerRadius = outerRadius - obj.strokeWidthPix;
+            gapSizePix = obj.rig.getDevice('Stage').um2pix(obj.currentGap);
+            
+            % Compute the angle corresponding to the gap size
+            circumference = 2 * pi * outerRadius;
+            gapAngle = (gapSizePix / circumference) * 2 * pi;
+            halfGapAngle = gapAngle / 2;
+            
+            [X, Y] = meshgrid(1:2*outerRadius, 1:2*outerRadius);
+            X = X - outerRadius;
+            Y = Y - outerRadius;
+            outerCircle = sqrt(X.^2 + Y.^2) <= outerRadius;
+            innerCircle = sqrt(X.^2 + Y.^2) <= innerRadius;
+            hollowCircle = outerCircle & ~innerCircle;
+            
+            % Create the gap centered on the right edge of the circle
+            gapMask = atan2(Y, X) > -halfGapAngle & atan2(Y, X) < halfGapAngle;
+            hollowCircle(gapMask) = 0;
+            
+            % Convert to image matrix
+            imageMatrix = obj.backgroundIntensity * ones(2*outerRadius, 2*outerRadius); % Background intensity
+            imageMatrix(hollowCircle) = obj.currentContrast*obj.backgroundIntensity + obj.backgroundIntensity; % Circle intensity
+
+            % Convert to uint8
+            imageMatrix = uint8(255*imageMatrix);
+        end
         
         function p = createPresentation(obj)
             p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
             p.setBackgroundColor(0.5);
             
             % Create the circle stimulus using Image
-            circle = stage.builtin.stimuli.Image();
-            circle.size = [obj.circleRadiusPix * 2, obj.circleRadiusPix * 2];
-            circle.position = [obj.canvasSize(1)/2, obj.canvasSize(2)/2];
+            image = stage.builtin.stimuli.Image(obj.computeImage());
+            image.size = [obj.circleRadiusPix * 2, obj.circleRadiusPix * 2];
+            image.position = [obj.canvasSize(1)/2, obj.canvasSize(2)/2];
             
             % Add the circle stimulus to the presentation
-            p.addStimulus(circle);
-            
-            % Create a PropertyController for the imageMatrix
-            imageController = stage.builtin.controllers.PropertyController(circle, 'imageMatrix', ...
-                @(state)setFrames(obj, state.time - obj.preTime * 1e-3));
-            p.addController(imageController);
+            p.addStimulus(image);
             
             % Create controllers for the stimuli visibility
-            circleVisible = stage.builtin.controllers.PropertyController(circle, 'visible', ...
+            imageVisible = stage.builtin.controllers.PropertyController(image, 'visible', ...
                 @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
-            p.addController(circleVisible);
-            function imageMatrix = setFrames(obj, time)
-                % Create a hollow circle with a gap
-                outerRadius = obj.circleRadiusPix;
-                innerRadius = outerRadius - obj.strokeWidthPix;
-                gapSizePix = obj.rig.getDevice('Stage').um2pix(obj.currentGap);
-                
-                % Compute the angle corresponding to the gap size
-                circumference = 2 * pi * outerRadius;
-                gapAngle = (gapSizePix / circumference) * 2 * pi;
-                halfGapAngle = gapAngle / 2;
-                
-                [X, Y] = meshgrid(1:2*outerRadius, 1:2*outerRadius);
-                X = X - outerRadius;
-                Y = Y - outerRadius;
-                outerCircle = sqrt(X.^2 + Y.^2) <= outerRadius;
-                innerCircle = sqrt(X.^2 + Y.^2) <= innerRadius;
-                hollowCircle = outerCircle & ~innerCircle;
-                
-                % Create the gap centered on the right edge of the circle
-                gapMask = atan2(Y, X) > -halfGapAngle & atan2(Y, X) < halfGapAngle;
-                hollowCircle(gapMask) = 0;
-                
-                % Convert to image matrix
-                imageMatrix = obj.backgroundIntensity * ones(2*outerRadius, 2*outerRadius); % Background intensity
-                imageMatrix(hollowCircle) = obj.currentContrast*obj.backgroundIntensity + obj.backgroundIntensity; % Circle intensity
-
-                % Convert to uint8
-                imageMatrix = uint8(255*imageMatrix);
-            end
+            p.addController(imageVisible);
         end
         
         function prepareEpoch(obj, epoch)
